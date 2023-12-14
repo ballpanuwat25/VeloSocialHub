970324b81b2423c5fb02890a5b0a135d0aebbea4
Ball Panuwat <79345283+ballpanuwat25@users.noreply.github.com>
2023-12-14 12:00:15 +0700

    Updated 14.12.2023

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
 import Post from '../components/Post'
+import Link from 'next/link'
 
 export default function Profile() {
   return (
@@ -29,7 +30,7 @@ export default function Profile() {
             <div className='w-full ml-72 py-8 pr-8 flex flex-row justify-between items-center'>
               <div>
                 <p className='text-3xl font-bold text-gray-800'>Ball Panuwat</p>
-                <p className='text-sm'>3 friends</p>
+                <p className='text-sm'>2 friends</p>
               </div>
 
               <div className='flex flex-row gap-2'>
@@ -107,7 +108,7 @@ export default function Profile() {
               <div className='flex flex-row items-center justify-between'>
                 <div className='flex flex-col'>
                   <h2 className='text-xl font-bold text-gray-800'>Friends</h2>
-                  <p className='text-xs text-gray-500'>3 friends</p>
+                  <p className='text-xs text-gray-500'>2 friends</p>
                 </div>
 
                 <button className='p-2 text-sm rounded hover:bg-gray-200 cursor-pointer'>
@@ -116,9 +117,18 @@ export default function Profile() {
               </div>
 
               <div className='grid grid-cols-3 gap-3'>
-                <p className='bg-red-300 w-full h-36 rounded'>1</p>
-                <p className='bg-red-300 w-full h-36 rounded'>2</p>
-                <p className='bg-red-300 w-full h-36 rounded'>3</p>
+                <div className='w-full h-36 rounded bg-cover bg-center bg-no-repeat'
+                  style={{
+                    backgroundImage: "url('https://images.unsplash.com/photo-1694434943114-c8ea2049f781?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                  }}
+                />
+
+                <div className='w-full h-36 rounded bg-cover bg-center bg-no-repeat'
+                  style={{
+                    backgroundImage: "url('https://images.unsplash.com/photo-1671293197723-31dbeaff1746?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                  }}
+                />
+
               </div>
             </div>
 
@@ -156,6 +166,195 @@ export default function Profile() {
 
                     Manage posts
                   </button>
+                </div>
+              </div>
+            </div>
+
+            <div className='w-full bg-white p-3 rounded flex flex-col gap-3'>
+              <div className='flex flex-row justify-between'>
+                <div className='flex flex-row gap-3'>
+                  <Link href={'/profile'} className='w-10 h-10 bg-primary rounded-full flex justify-center items-center hover:bg-rose-500'>
+                    <div
+                      className='w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat cursor-pointer'
+                      style={{
+                        backgroundImage: "url('https://images.unsplash.com/photo-1638803040283-7a5ffd48dad5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                      }}
+                    />
+                  </Link>
+
+                  <div className='flex flex-col'>
+                    <a className='font-medium text-sm hover:underline hover:cursor-pointer'>Ball Panuwat <span className='font-light text-sm'>updated his profile picture.</span></a>
+                    <div className='flex flex-row gap-1 items-center'>
+                      <p className='text-xs'>November 2 ·</p>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
+                        <path fill='#374151' d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='flex flex-row'>
+                  <button className='w-10 h-10 rounded-full flex justify-center items-center hover:bg-gray-50'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
+                      <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" fill="#E33450" />
+                    </svg>
+                  </button>
+
+                  <button className='w-10 h-10 rounded-full flex justify-center items-center hover:bg-gray-50'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
+                      <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" fill="#E33450" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <hr />
+
+              <div
+                className='aspect-square bg-cover bg-center bg-no-repeat bg-gray-100 rounded'
+                style={{
+                  backgroundImage: "url('https://images.unsplash.com/photo-1638803040283-7a5ffd48dad5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                }}
+              />
+
+              <div className='flex flex-row justify-between'>
+                <div className='flex flex-row items-center gap-2'>
+                  <button className='cursor-pointer'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                      <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" fill="#E33450" />
+                    </svg>
+                  </button>
+                  <span className='text-primary hover:underline cursor-pointer text-sm'> 2</span>
+                </div>
+
+                <div className='flex flex-row gap-5'>
+                  <div className='flex flex-row items-center gap-2'>
+                    <button className='cursor-pointer'>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                        <path d="M512 240c0 114.9-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6C73.6 471.1 44.7 480 16 480c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4l0 0 0 0 0 0 0 0 .3-.3c.3-.3 .7-.7 1.3-1.4c1.1-1.2 2.8-3.1 4.9-5.7c4.1-5 9.6-12.4 15.2-21.6c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208z" fill="#E33450" />
+                      </svg>
+                    </button>
+                    <span className='text-primary hover:underline cursor-pointer text-sm'> 0</span>
+                  </div>
+
+                  <div className='flex flex-row items-center gap-2'>
+                    <button className='cursor-pointer'>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                        <path d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z" fill="#E33450" />
+                      </svg>
+                    </button>
+                    <span className='text-primary hover:underline cursor-pointer text-sm'> 0</span>
+                  </div>
+                </div>
+              </div>
+
+              <hr />
+
+              <div className='flex flex-row justify-evenly gap-5'>
+                <div className='flex flex-row gap-2 cursor-pointer'>
+                  <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                      <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" fill="#E33450" />
+                    </svg>
+                  </button>
+                  <p className='text-primary'>Like</p>
+                </div>
+
+                <div className='flex flex-row gap-2 cursor-pointer'>
+                  <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                      <path d="M512 240c0 114.9-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6C73.6 471.1 44.7 480 16 480c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4l0 0 0 0 0 0 0 0 .3-.3c.3-.3 .7-.7 1.3-1.4c1.1-1.2 2.8-3.1 4.9-5.7c4.1-5 9.6-12.4 15.2-21.6c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208z" fill="#E33450" />
+                    </svg>
+                  </button>
+                  <p className='text-primary'>Comment</p>
+                </div>
+
+                <div className='flex flex-row gap-2 cursor-pointer'>
+                  <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                      <path d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z" fill="#E33450" />
+                    </svg>
+                  </button>
+                  <p className='text-primary'>Share</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='w-full bg-white p-3 rounded flex flex-col gap-3 mb-3'>
+              <div className='flex flex-row justify-between'>
+                <div className='flex flex-row gap-3'>
+                  <Link href={'/profile'} className='w-10 h-10 bg-primary rounded-full flex justify-center items-center hover:bg-rose-500'>
+                    <div
+                      className='w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat cursor-pointer'
+                      style={{
+                        backgroundImage: "url('https://images.unsplash.com/photo-1638803040283-7a5ffd48dad5?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                      }}
+                    />
+                  </Link>
+
+                  <div className='flex flex-col'>
+                    <a className='font-medium text-sm hover:underline hover:cursor-pointer'>Ball Panuwat <span className='font-light text-sm'>updated his cover picture.</span></a>
+                    <div className='flex flex-row gap-1 items-center'>
+                      <p className='text-xs'>November 2 ·</p>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="12" width="12" viewBox="0 0 512 512">
+                        <path fill='#374151' d="M352 256c0 22.2-1.2 43.6-3.3 64H163.3c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64H348.7c2.2 20.4 3.3 41.8 3.3 64zm28.8-64H503.9c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64H380.8c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32H376.7c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0H167.7c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0H18.6C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192H131.2c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64H8.1C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6H344.3c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352H135.3zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6H493.4z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='flex flex-row'>
+                  <button className='w-10 h-10 rounded-full flex justify-center items-center hover:bg-gray-50'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512">
+                      <path d="M8 256a56 56 0 1 1 112 0A56 56 0 1 1 8 256zm160 0a56 56 0 1 1 112 0 56 56 0 1 1 -112 0zm216-56a56 56 0 1 1 0 112 56 56 0 1 1 0-112z" fill="#E33450" />
+                    </svg>
+                  </button>
+
+                  <button className='w-10 h-10 rounded-full flex justify-center items-center hover:bg-gray-50'>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
+                      <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" fill="#E33450" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <hr />
+
+              <div
+                className='aspect-video bg-cover bg-center bg-no-repeat bg-gray-100 rounded'
+                style={{
+                  backgroundImage: "url('https://images.unsplash.com/photo-1666030433251-f98e43be8241?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                }}
+              />
+
+              <hr />
+
+              <div className='flex flex-row justify-evenly gap-5'>
+                <div className='flex flex-row gap-2 cursor-pointer'>
+                  <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                      <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" fill="#E33450" />
+                    </svg>
+                  </button>
+                  <p className='text-primary'>Like</p>
+                </div>
+
+                <div className='flex flex-row gap-2 cursor-pointer'>
+                  <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                      <path d="M512 240c0 114.9-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6C73.6 471.1 44.7 480 16 480c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4l0 0 0 0 0 0 0 0 .3-.3c.3-.3 .7-.7 1.3-1.4c1.1-1.2 2.8-3.1 4.9-5.7c4.1-5 9.6-12.4 15.2-21.6c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208z" fill="#E33450" />
+                    </svg>
+                  </button>
+                  <p className='text-primary'>Comment</p>
+                </div>
+
+                <div className='flex flex-row gap-2 cursor-pointer'>
+                  <button>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                      <path d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z" fill="#E33450" />
+                    </svg>
+                  </button>
+                  <p className='text-primary'>Share</p>
                 </div>
               </div>
             </div>
